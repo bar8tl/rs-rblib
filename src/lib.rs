dc3ba80ee9042fb48231c6ca6d274a44c5624fa2
bar8tl <ricardoe.barrera@gmail.com>
2023-11-07 13:56:17 -0600

    v2.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // rblib: Rust library with common functions used in bar8tl programs
 pub mod create_sqlite3_table;
 pub mod create_sqlite3_tablelist;
+pub mod move_file_wf;
 pub mod number_in_list;
 pub mod pass_filter;
 pub mod print_type_of;
---
+++ src/move_file_wf.rs
@@ -0,0 +1,11 @@
+// move_file_wf: Move a file between directories as part of a workflow process
+use std::fs;
+
+pub fn move_file_wf(mode: &str, curdir: &str, pcddir: &str, filenm: &str,
+  filetp: &str) {
+  let oldnm = format!("{}{}.{}", curdir, filenm, filetp);
+  if mode == "inp" {
+    let newnm = format!("{}{}.{}", pcddir, filenm, filetp);
+    fs::rename(oldnm, newnm).expect("File rename failure");
+  }
+}
