2b6dd51ba36ccf3b0f7e23bfb0e26cd6534c5e6c
bar8tl <ricardoe.barrera@gmail.com>
2025-05-29 10:27:31 -0600

    v1.0.1

--- src/build.bat
+++ src/build.bat
@@ -1,6 +1,3 @@
-cd c:\rbhome\rust\rblib\src
-xcopy c:\cprogs\rblib-rs\src\*.rs   . /D /V /C /F /Y
-cd ..
-xcopy c:\cprogs\rblib-rs\cargo.toml . /D /V /C /F /Y
-cargo build
+cd c:\rbhome\rust\rblib
+cargo build --release
 pause
